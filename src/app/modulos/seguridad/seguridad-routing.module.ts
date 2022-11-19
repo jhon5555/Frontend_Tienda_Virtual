a440950b0385a43cbfefa260334622f393bc7f45
DobierTeb <116413549+DobierTeb@users.noreply.github.com>
2022-11-18 23:42:39 -0500

    Rutas para los cambios de clave

--- src/app/modulos/seguridad/seguridad-routing.module.ts
+++ src/app/modulos/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
 import { CerrarSesionComponent } from './cerrar-sesion/cerrar-sesion.component';
 import { IdentificacionComponent } from './identificacion/identificacion.component';
+import { RecuperarClaveComponent } from './recuperar-clave/recuperar-clave.component';
 
 const routes: Routes = [
   {
@@ -11,7 +13,17 @@ const routes: Routes = [
   {
     path: "cerrarSesion",
     component: CerrarSesionComponent
+  },
+  {
+    path: "cambio-clave",
+    component: CambioClaveComponent
+  },
+
+  {
+    path: "cambioClave",
+    component: RecuperarClaveComponent
   }
+
 ];
 
 @NgModule({
